0ff19000d025645ad94659e7a4bf91af6a8cf986
ReZaUL ALaM ONi <rzalam93@gmail.com>
2019-10-29 22:26:22 +0600

    design update

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -58,85 +58,37 @@
 
     <section class="header-wrapper navgiation-wrapper">
 
+        <
         <div class="navbar navbar-default">
             <div class="container">
-
                 <div class="navbar-header">
                     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span>
                     </button>
-                    <a class="logo" href="index.html"><img alt="" src="images/logo.png"></a>
-                </div>
-
+                    <a class="logo" href="https://saptagaon.com/">
+                        <img style="height: 60px;" alt="" src={{}}""> </a></div>
                 <div class="navbar-collapse collapse">
                     <ul class="nav navbar-nav navbar-right">
-
-                        <li>
-                            <a href="#">Home</a>
+                        <li><a href="#" title="Excutive Committee">Excutive Committee</a>
                             <ul class="drop-down">
-                                <li><a href="index.html" title="Home Layout 01">Home 01</a></li>
-                                <li><a href="home-2.html" title="Home Layout 02">Home 02</a></li>
+                                <li><a href="https://saptagaon.com/advisers">Advisers</a></li>
+                                <li><a href="https://saptagaon.com/excutive-committee">Excutive Committee</a></li>
                             </ul>
                         </li>
-
-
-                        <li><a href="about-us.html" title="About Us">About Us</a></li>
-
-                        <li>
-                            <a href="#">Campaign</a>
+                        <li><a href="#" title="Members">Members</a>
                             <ul class="drop-down">
-                                <li><a href="events.html">All Campaigns</a></li>
-                                <li><a href="event-single.html">Single Campaign</a></li>
+                                <li><a href="https://saptagaon.com/about-membership">About Membership</a></li>
+                                <li><a href="#">Apply for Membership</a></li>
+                                <li><a href="#">Life Member</a></li>
+                                <li><a href="https://saptagaon.com/founder-member">Founder Member</a></li>
                             </ul>
                         </li>
-
-                        <li class="drop"><a href="#">Pages</a>
-                            <ul class="drop-down">
-
-                                <li class="drop"><a href="#">Elements</a>
-                                    <ul class="drop-down level3">
-                                        <li><a href="element-cta.html">CTA/PROMO BOX</a></li>
-                                        <li><a href="element-counter.html">Fun Facts</a></li>
-                                        <li><a href="element-logos.html">Logos/Clients</a></li>
-                                        <li><a href="element-process.html">Process</a></li>
-                                        <li><a href="element-highlights.html">Highlights Box</a></li>
-                                        <li><a href="element-service.html">Services</a></li>
-                                        <li><a href="element-subscribe.html">Subscribe</a></li>
-                                        <li><a href="element-team.html">Team</a></li>
-                                        <li><a href="element-feedback.html">Testimonials</a></li>
-                                    </ul>
-                                </li>
-
-                                <li class="drop"><a href="#">Gallery</a>
-                                    <ul class="drop-down level3">
-                                        <li><a href="gallery-1.html">Layout 01</a></li>
-                                        <li><a href="gallery-2.html">Layout 02</a></li>
-
-                                    </ul>
-                                </li>
-                                <li><a href="faq.html" title="FAQ">FAQ</a></li>
-                                <li><a href="404.html" title="404 Page">404 Page</a></li>
-                                <li class="drop"><a href="#">Level 3</a>
-                                    <ul class="drop-down level3">
-                                        <li><a href="#">Level 3.1</a></li>
-                                        <li><a href="#">Level 3.2</a></li>
-                                        <li><a href="#">Level 3.3</a></li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </li>
-
-                        <li>
-                            <a href="#">Blog</a>
-                            <ul class="drop-down">
-                                <li><a href="blog.html">All Posts</a></li>
-                                <li><a href="single.html">Single Page</a></li>
-                            </ul>
-                        </li>
-
-                        <li><a href="contact.html">Contact</a></li>
+                        <li><a href="https://saptagaon.com/about-us" title="About Us">About Us</a></li>
+                        <!--<li><a href="https://saptagaon.com/blog">Blog</a></li>-->
+                        <li><a href="https://saptagaon.com/blog">gallary</a></li>
+                        <li><a href="https://saptagaon.com/contact-us">Contact</a></li>
+                        <li><a href="https://saptagaon.com/registration">Join as Donor</a></li>
+                        <li><a href="https://saptagaon.com/login">Login</a></li>
                     </ul>
                 </div>
             </div>
