fdaea544999be582eb63e3f97ed0fb8c0dc739d6
RezaulAlamOni <rzalam93@gmail.com>
2022-03-08 16:57:19 +0600

    Update code for new design

--- app/Http/Controllers/VolunteerController.php
+++ app/Http/Controllers/VolunteerController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Imports\UsersImport;
+use App\Models\BloodGroup;
 use App\Models\Volunteer;
+use App\Models\VolunteerArea;
 use App\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 //use Maatwebsite\Excel\Facades\Excel;
 use Excel;
+use Illuminate\Support\Facades\Hash;
 
 class VolunteerController extends Controller
 {
@@ -51,10 +55,45 @@ class VolunteerController extends Controller
     {
         $path = $request->file('file')->getRealPath();
         $data = Excel::load($path)->get();
+        $data__ = [];
         foreach ($data as $datum) {
-            
-        }
 
+            try {
+                $a_id = VolunteerArea::query()->where('name',$datum->area)->first();
+                $a_id = $a_id ? $a_id->id : null;
+                $b_grp = BloodGroup::query()->where('name',$datum->blood)->first();
+                $bg_id = $b_grp ? $b_grp->id  : null;
+
+                $user =  User::updateOrCreate([
+                    'email' => trim($datum->email??$datum->phone)
+                ],
+                    [
+                        'type' => 'volunteer',
+                        'name' => $datum->name,
+                        'email' => trim($datum->email??$datum->phone),
+                        'blood_group_id' => $bg_id,
+                        'areas_id' => $a_id,
+                        'phone' => $datum->phone,
+                        'address' => '',
+                        'password' => Hash::make($datum->phone),
+                    ]);
+
+                Volunteer::updateOrInsert(['user_id' => $user->id],[
+                    'user_id' => $user->id,
+                    'v_area_id' => $a_id,
+                    'v_type' => '',
+                    'status' => 0,
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now()
+                ]);
+            } catch (\Exception $exception){
+                $data__[] = $datum;
+            }
+        }
+        if (count($data__)  > 0) {
+            return response()->json(['status' => 'success','data' => $data__]);
+        }
+        return response()->json(['status' => 'success']);
     }
 
 
